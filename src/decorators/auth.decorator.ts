5f922fc6ceaf58d1a30f692291d45a0c26b1707b
ckyeon <qsky8520@gmail.com>
2022-07-14 13:35:30 +0900

    Add ApiException for Swagger Docs

--- src/decorators/auth.decorator.ts
+++ src/decorators/auth.decorator.ts
@@ -1,7 +1,14 @@
 import { applyDecorators, UseFilters, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guards/jwt.guard';
 import { JwtExceptionFilter } from '../filters/jwt-exception.filter';
+import { ApiException } from './api-exception.decorator';
+import { InvalidAccessTokenException } from '../auth/exceptions/invalid-access-token.exception';
+import { AccessTokenExpiredException } from '../auth/exceptions/access-token-expired.exception';
 
 export function Auth() {
-  return applyDecorators(UseGuards(JwtGuard), UseFilters(JwtExceptionFilter));
+  return applyDecorators(
+    UseGuards(JwtGuard),
+    UseFilters(JwtExceptionFilter),
+    ApiException(AccessTokenExpiredException, InvalidAccessTokenException),
+  );
 }
