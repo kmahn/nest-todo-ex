d07de8a2f24f2ba581fbe683d2a1012cedd91773
ckyeon <qsky8520@gmail.com>
2022-07-14 11:26:08 +0900

    Update NestHttpException to CustomHttpException

--- src/filters/jwt-exception.filter.ts
+++ src/filters/jwt-exception.filter.ts
@@ -3,8 +3,11 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { HttpExceptionResponse } from '../types/exception';
+import { ErrorCodes } from '../errors/error-definition';
 
 @Catch(HttpException)
 export class JwtExceptionFilter implements ExceptionFilter {
@@ -15,14 +18,13 @@ export class JwtExceptionFilter implements ExceptionFilter {
     const status = exception.getStatus();
 
     const { message } = request.authInfo as Error;
-    let code = 'invalid access-token';
+    let code = ErrorCodes.INVALID_ACCESS_TOKEN;
     if (message === 'jwt expired') {
-      code = 'access-token expired';
+      code = ErrorCodes.ACCESS_TOKEN_EXPIRED;
     }
-
     response.status(status).json({
-      status,
+      status: status,
       code,
-    });
+    } as HttpExceptionResponse);
   }
 }
--- src/users/commands/create-user.command.handler.ts
+++ src/users/commands/create-user.command.handler.ts
@@ -1,10 +1,10 @@
-import { BadRequestException } from '@nestjs/common';
 import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UserDocument } from '../../infra/database/models/user.model';
 import { CreateUserEvent } from '../events/create-user.event';
 import { CreateUserCommand } from './create-user.command';
+import { EmailAlreadyInUseException } from '../exceptions/email-already-in-use.exception';
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserCommandHandler
@@ -19,7 +19,7 @@ export class CreateUserCommandHandler
     const { email, name, phone, password } = command;
     const exUser = await this.userModel.findOne({ email });
     if (!!exUser) {
-      throw new BadRequestException('사용하고 있는 이메일입니다.');
+      throw new EmailAlreadyInUseException();
     }
     const userDocument = await this.userModel.create({ email, name, phone });
     this.eventBus.publish(
