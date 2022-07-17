22a7bb4b9c54046277128389050fd685186db852
ckyeon <qsky8520@gmail.com>
2022-07-17 19:38:22 +0900

    Update ApiException to ApiExceptions for same status code in Swagger

--- src/decorators/auth.decorator.ts
+++ src/decorators/auth.decorator.ts
@@ -1,14 +1,14 @@
 import { applyDecorators, UseFilters, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../auth/guards/jwt.guard';
 import { JwtExceptionFilter } from '../filters/jwt-exception.filter';
-import { ApiException } from './api-exception.decorator';
 import { InvalidAccessTokenException } from '../auth/exceptions/invalid-access-token.exception';
 import { AccessTokenExpiredException } from '../auth/exceptions/access-token-expired.exception';
+import { ApiExceptions } from './api-exceptions.decorator';
 
 export function Auth() {
   return applyDecorators(
     UseGuards(JwtGuard),
     UseFilters(JwtExceptionFilter),
-    ApiException(AccessTokenExpiredException, InvalidAccessTokenException),
+    ApiExceptions(AccessTokenExpiredException, InvalidAccessTokenException),
   );
 }
